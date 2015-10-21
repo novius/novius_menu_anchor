9840c0825b1f3e01e386d66504ce7974ae9d2681
Martin Smeeckaert <smeeckaert@novius.fr>
2015-10-21 17:42:05 +0200

    set url origin

--- views/menu/driver/anchor.view.php
+++ views/menu/driver/anchor.view.php
@@ -1,7 +1,8 @@
 <?php
 
 if (isset($item_driver->item) && isset($item_driver->item->mitem__anchor)) {
-    $url = \Arr::get($params, 'base_url_anchor', '');
+    $url = \Arr::get($params, 'base_url_anchor', \Nos\Nos::main_controller()->getUrl());
+
     ?>
     <a href="<?= $url ?>#<?= $item_driver->item->mitem__anchor ?>"
        title="<?= e($item_driver->title()) ?>"><?= e($item_driver->title()) ?></a>
